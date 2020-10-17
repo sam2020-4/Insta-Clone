e94b0410a8c648ba99a7a3e21b6c5db472513782
matata samuel <mattasamuel3@gmail.com>
2020-10-17 20:14:47 +0300

    inserted index function on views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    date = dt.date.today()
+    images = Image.get_images()
+    comments = Comment.get_comment()
+    
+    current_user = request.user 
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, auto_id=False)
+        img_id = request.POST['image_id']
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = current_user
+            image = Image.get_image(img_id)
+            comment.image = image
+            comment.save()
+        return redirect(f'/#{img_id}',)
+    else:
+        form = NewCommentForm(auto_id=False)
+
+    return render(request, 'index.html', {"date": date, "images":images, "comments":comments, "form": form,})
+
+
