a59a9a60f25d8381a5616784a241675ba880473b
matata samuel <mattasamuel3@gmail.com>
2020-10-17 20:29:04 +0300

    inserted profile templ

--- photos/views.py
+++ photos/views.py
@@ -99,6 +99,25 @@ def new_image(request):
     return render(request, 'postnew.html', {"form": form})
 
 
+@login_required(login_url='/accounts/login/')
+def user_profiles(request):
+    current_user = request.user
+    Author = current_user
+    images = Image.get_by_author(Author)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+        
+    else:
+        form = ProfileUpdateForm()
+    
+    return render(request, 'registration/profile.html', {"form":form, "images":images})
+
+
 
 
 
