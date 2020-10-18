e6fff773b3c8043c71d5ef75a23619655445fb9f
matata samuel <mattasamuel3@gmail.com>
2020-10-18 03:33:27 +0300

    added form content

--- photos/signals.py
+++ photos/signals.py
@@ -4,12 +4,10 @@ from django.dispatch import receiver
 from .models import Profile
 from .email import send_welcome_email
 
-
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-        
+        Profile.objects.create(user=instance)        
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
